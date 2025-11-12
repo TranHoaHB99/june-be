40493d46833c7fd55a20818d3ed3e21a7958b2cd
Kris Tran <tranhoahb99@gmail.com>
2025-11-12 15:55:34 +0700

    up

--- config/swagger.js
+++ config/swagger.js
@@ -16,6 +16,10 @@ const options = {
         url: 'http://localhost:3000',
         description: 'Development server',
       },
+      {
+        url: 'https://june-be.onrender.com',
+        description: 'Production server',
+      },
     ],
     components: {
       securitySchemes: {
